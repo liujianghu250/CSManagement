7055e7f98db22737a072474c7e7809a0cd790a1e
liujianghu250 <1822693172@qq.com>
2020-05-13 17:08:45 +0800

    add local files

---
+++ WindowsFormsApp1/LoginForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            MainForm frm = new MainForm();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //测试自己的模块时将LoginForm改成自己的窗口的名字即可。
+            Application.Run(new LoginForm());
+        }
+    }
+}
